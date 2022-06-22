3bbd9d045f49e91dc64c54dece55d14ffec8fdfa
Vladislav2509 <VladislavDultev@mail.ru>
2022-06-22 15:27:45 +0300

    homework_33

---
+++ homework_33/app.js
@@ -0,0 +1,73 @@
+
+
+const users = [
+    {
+    id: 1,
+    username: "Michael Lawson",
+    age: 22,
+    },
+    {
+    id: 2,
+    username: "Tom Spot",
+    age: 32,
+    },
+    {
+    id: 3,
+    username: "Kate Ford",
+    age: 18,
+    }
+   ];
+
+
+//    Task_1 AGE
+
+//    function getAverageAge(users) {
+//     return users.reduce((prev, user) => prev + user.age, 0) / users.length;
+//   }
+
+
+//     let Michael = { username: "Michael Lawson", age: 22 };
+//     let Tom = { username: "Tom Spot", age: 32 };
+//     let Kate = { username: "Kate Ford", age: 18 };
+
+//     let arr = [ Michael, Tom, Kate ];
+
+// console.log(getAverageAge(arr)); 
+
+
+// Task_2 SORT
+
+// function sortByAge(arr) {
+//     arr.sort((a, b) => a.age < b.age ? 1 : -1);
+//   }
+  
+//   let Michael = { username: "Michael Lawson", age: 22 };
+//   let Tom = { username: "Tom Spot", age: 32 };
+//   let Kate = { username: "Kate Ford", age: 18 };
+  
+//   let arr = [ Michael, Tom, Kate ];
+  
+//   sortByAge(arr);
+  
+//   console.log(arr[0].username); 
+//   console.log(arr[1].username); 
+//   console.log(arr[2].username); 
+
+
+// Task 3
+// let age = prompt ("How old are you?");
+// function checkAgeUser () {
+//     if (age >= 22) {
+//     console.log("true");
+// } else if (age < 22) {
+//     console.log("false");
+// }    
+    
+// }
+
+// checkAgeUser()
+
+
+
+
+
